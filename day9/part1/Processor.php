77848381806821d8e3a8c4ec7fa14d27c355fbea
xonnex <jane98@gmx.ch>
2017-11-09 22:45:19 +0100

    Finished day9 part1

---
+++ day9/part1/Processor.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Created by PhpStorm
+ * @author Janik Baumann <baumannjanik@gmail.com>
+ * @copyright 2014-2017 Janik Baumann
+ */
+class Processor
+{
+
+    /**
+     * @param string $input Compressed data
+     * @return int Decompressed length
+     */
+    public function process(string $input): int
+    {
+        $output = '';
+
+        while (strlen($input) > 0) {
+            preg_match_all('/^([A-Z]*)|^\(([0-9]*)x([0-9]*)\)/', $input, $matches, PREG_SET_ORDER);
+
+            if (isset($matches[1])) {
+                $length = (int)$matches[1][2];
+                $times = (int)$matches[1][3];
+                $input = substr($input, strlen($matches[1][0]));
+                $sub = substr($input, 0, $length);
+                $output .= str_repeat($sub, $times);
+                $input = substr($input, $length);
+            } else {
+                $output .= $matches[0][0];
+                $input = substr($input, strlen($matches[0][0]));
+            }
+        }
+
+        $output = str_replace(' ', '', $output);
+        return strlen($output);
+    }
+}
--- day9/part1/test.php
+++ day9/part1/test.php
@@ -6,10 +6,10 @@
  */
 declare(strict_types=1);
 
-include __DIR__ . '/Solution.php';
+include __DIR__ . '/Processor.php';
 
-$solution = new Solution();
-$res = $solution->process(file_get_contents(__DIR__ . '/input_example_1.txt'));
+$processor = new Processor();
+$res = $processor->process(file_get_contents(__DIR__ . '/input_example_1.txt'));
 
 if ($res === 6) {
     echo 'Example 1: Passed' . PHP_EOL;
@@ -17,8 +17,8 @@ if ($res === 6) {
     echo 'Example 1: Failed' . PHP_EOL;
 }
 
-$solution = new Solution();
-$res = $solution->process(file_get_contents(__DIR__ . '/input_example_2.txt'));
+$processor = new Processor();
+$res = $processor->process(file_get_contents(__DIR__ . '/input_example_2.txt'));
 
 if ($res === 7) {
     echo 'Example 2: Passed' . PHP_EOL;
@@ -26,8 +26,8 @@ if ($res === 7) {
     echo 'Example 2: Failed' . PHP_EOL;
 }
 
-$solution = new Solution();
-$res = $solution->process(file_get_contents(__DIR__ . '/input_example_3.txt'));
+$processor = new Processor();
+$res = $processor->process(file_get_contents(__DIR__ . '/input_example_3.txt'));
 
 if ($res === 9) {
     echo 'Example 3: Passed' . PHP_EOL;
@@ -35,8 +35,8 @@ if ($res === 9) {
     echo 'Example 3: Failed' . PHP_EOL;
 }
 
-$solution = new Solution();
-$res = $solution->process(file_get_contents(__DIR__ . '/input_example_4.txt'));
+$processor = new Processor();
+$res = $processor->process(file_get_contents(__DIR__ . '/input_example_4.txt'));
 
 if ($res === 11) {
     echo 'Example 4: Passed' . PHP_EOL;
@@ -44,8 +44,8 @@ if ($res === 11) {
     echo 'Example 4: Failed' . PHP_EOL;
 }
 
-$solution = new Solution();
-$res = $solution->process(file_get_contents(__DIR__ . '/input_example_5.txt'));
+$processor = new Processor();
+$res = $processor->process(file_get_contents(__DIR__ . '/input_example_5.txt'));
 
 if ($res === 6) {
     echo 'Example 5: Passed' . PHP_EOL;
@@ -53,8 +53,8 @@ if ($res === 6) {
     echo 'Example 5: Failed' . PHP_EOL;
 }
 
-$solution = new Solution();
-$res = $solution->process(file_get_contents(__DIR__ . '/input_example_6.txt'));
+$processor = new Processor();
+$res = $processor->process(file_get_contents(__DIR__ . '/input_example_6.txt'));
 
 if ($res === 18) {
     echo 'Example 6: Passed' . PHP_EOL;
