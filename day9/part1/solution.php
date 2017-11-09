28cd5d063eb5b871ae42fbc967d588d163097219
xonnex <jane98@gmx.ch>
2017-11-09 22:45:42 +0100

    Finished day9 part1

---
+++ day9/part1/solution.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm
+ * @author Janik Baumann <baumannjanik@gmail.com>
+ * @copyright 2014-2017 Janik Baumann
+ */
+
+declare(strict_types=1);
+
+include __DIR__ . '/Processor.php';
+
+$processor = new Processor();
+$res = $processor->process(file_get_contents(__DIR__ . '/input.txt'));
+
+file_put_contents(__DIR__ . '/output.txt', $res);
